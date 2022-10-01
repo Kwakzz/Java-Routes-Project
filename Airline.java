1b6f55c5ef2985a470d92e171d41e46488ae3efa
Kwakzz <46132576+Kwakzz@users.noreply.github.com>
2022-10-01 23:25:27 +0000

    Update Airline.java

--- Airline.java
+++ Airline.java
@@ -20,35 +20,62 @@ public class Airline {
         this.airlineCode = airlineCode;
     }
 
-
+    /**
+     * @return name of airline used for flight
+     */
     public String getAirlineName() {
         return airlineName;
     }
 
+    /**
+     * sets name of airline to airlineName
+     * @param airlineName the name of the airline used for flight
+     */
     public void setAirlineName(String airlineName) {
         this.airlineName = airlineName;
     }
 
+    /**
+     * @return airline code
+     */
     public String getAirlineCode() {
         return airlineCode;
     }
 
+    /**
+     * set code of airline used for flight to airlineCode
+     * @param airlineCode code of airline used for flight
+     */
     public void setAirlineCode(String airlineCode) {
         this.airlineCode = airlineCode;
     }
 
+    /**
+     * @return id of airline used for flight
+     */
     public String getAirlineID() {
         return airlineID;
     }
 
+    /**
+     * sets ID of airline used for flight to airlineID
+     * @param airlineID
+     */
     public void setAirlineID(String airlineID) {
         this.airlineID = airlineID;
     }
 
+    /**
+     * @return number of stops during flight
+     */
     public int getNoOfStops() {
         return noOfStops;
     }
 
+    /**
+     * set number of stops during flight to noOfStops
+     * @param noOfStops
+     */
     public void setNoOfStops(int noOfStops) {
         this.noOfStops = noOfStops;
     }
